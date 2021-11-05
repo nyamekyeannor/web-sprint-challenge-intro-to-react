62ab1fb9df44ec34cabe5c7c23013a5ba84f51f2
Nyamekye Annor <nyamekyeannor@Nyamekyes-MacBook-Pro.local>
2021-11-05 11:11:53 -0500

    SECOND PUSH

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+import "./Character.css";
+
+function Character(props) {
+  return (
+    // props.users.map(user => (
+    //     <Character user = {user} key = {}
+    // ))
+    <div className="character-list">
+      <div className="character-list__name">
+        <h2>characterName</h2>
+        <div className="character-list__number">characterNumber</div>
+      </div>
+    </div>
+  );
+}
+
+export default Character;
